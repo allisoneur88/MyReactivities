ecabe254366e8969555b25edc980a5074860499a
allisoneur88 <adganshin@gmail.com>
2025-04-08 17:03:33 +0200

    8_Added a little bit of MobX state for a loading indicator

---
+++ client/src/lib/stores/uiStore.ts
@@ -0,0 +1,17 @@
+import { makeAutoObservable } from "mobx";
+
+export class UiStore {
+   isLoading = false;
+
+   constructor() {
+      makeAutoObservable(this);
+   }
+
+   setBusy() {
+      this.isLoading = true;
+   }
+
+   setIdle() {
+      this.isLoading = false;
+   }
+}
