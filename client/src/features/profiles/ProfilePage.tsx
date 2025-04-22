96ecd8249b1834a7bab3d4e3bf4b59c237d7f6fc
allisoneur88 <adganshin@gmail.com>
2025-04-22 17:20:20 +0200

    16_1_Finished with Photo Upload functionality on client side. Little ui polishes

--- client/src/features/profiles/ProfilePage.tsx
+++ client/src/features/profiles/ProfilePage.tsx
@@ -1,14 +1,25 @@
-import { Grid2 } from "@mui/material";
+import { Grid2, Typography } from "@mui/material";
 import ProfileHeader from "./ProfileHeader";
 import ProfileContent from "./ProfileContent";
+import { useParams } from "react-router";
+import { useProfile } from "../../lib/hooks/useProfile";
 
 export default function ProfilePage() {
-   return (
-      <Grid2 container>
-         <Grid2 size={12}>
-            <ProfileHeader />
-            <ProfileContent />
-         </Grid2>
-      </Grid2>
-   )
+
+    const { id } = useParams();
+
+    const { profile, loadingProfile } = useProfile(id);
+
+    if (loadingProfile) return <Typography>Loading profile...</Typography>
+
+    if (!profile) return <Typography>Profile not found</Typography>
+
+    return (
+        <Grid2 container>
+            <Grid2 size={12}>
+                <ProfileHeader profile={profile} />
+                <ProfileContent />
+            </Grid2>
+        </Grid2>
+    )
 }
--- client/src/lib/hooks/useAccount.ts
+++ client/src/lib/hooks/useAccount.ts
@@ -1,68 +1,65 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { LoginSchema } from "../schemas/loginSchema"
 import agent from "../api/agent"
-import { useLocation, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { RegisterSchema } from "../schemas/registerSchema";
 import { toast } from "react-toastify";
 
 export const useAccount = () => {
 
-   const queryClient = useQueryClient();
+    const queryClient = useQueryClient();
 
-   const navigate = useNavigate();
-   const location = useLocation();
+    const navigate = useNavigate();
 
-   const registerUser = useMutation({
-      mutationFn: async (creds: RegisterSchema) => {
-         await agent.post("/account/register", creds)
-      },
+    const registerUser = useMutation({
+        mutationFn: async (creds: RegisterSchema) => {
+            await agent.post("/account/register", creds)
+        },
 
-      onSuccess: () => {
-         toast.success("Register successful -  you can now login");
-         navigate("/login");
-      }
-   })
+        onSuccess: () => {
+            toast.success("Register successful -  you can now login");
+            navigate("/login");
+        }
+    })
 
-   const loginUser = useMutation({
-      mutationFn: async (creds: LoginSchema) => {
-         await agent.post("/login?useCookies=true", creds)
-      },
-      onSuccess: async () => {
-         await queryClient.invalidateQueries({
-            queryKey: ["user"]
-         });
-      }
-   });
+    const loginUser = useMutation({
+        mutationFn: async (creds: LoginSchema) => {
+            await agent.post("/login?useCookies=true", creds)
+        },
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ["user"]
+            });
+        }
+    });
 
-   const logoutUser = useMutation({
-      mutationFn: async () => {
-         await agent.post("/account/logout");
-      },
+    const logoutUser = useMutation({
+        mutationFn: async () => {
+            await agent.post("/account/logout");
+        },
 
-      onSuccess: () => {
-         queryClient.removeQueries({ queryKey: ["user"] });
-         queryClient.removeQueries({ queryKey: ["activities"] });
-         navigate("/");
-      }
-   })
+        onSuccess: () => {
+            queryClient.removeQueries({ queryKey: ["user"] });
+            queryClient.removeQueries({ queryKey: ["activities"] });
+            navigate("/");
+        }
+    })
 
-   const { data: currentUser, isLoading: loadingUserInfo } = useQuery({
-      queryKey: ["user"],
-      queryFn: async () => {
-         const response = await agent.get<User>("/account/user-info");
-         return response.data;
-      },
-      enabled: !queryClient.getQueryData(["user"])
-         && location.pathname !== "/login"
-         && location.pathname !== "/register"
-   })
+    const { data: currentUser, isLoading: loadingUserInfo } = useQuery({
+        queryKey: ["user"],
+        queryFn: async () => {
+            const response = await agent.get<User>("/account/user-info");
+            return response.data;
+        },
+        enabled: !queryClient.getQueryData(["user"])
+    })
 
 
-   return {
-      registerUser,
-      loginUser,
-      currentUser,
-      logoutUser,
-      loadingUserInfo
-   }
+    return {
+        registerUser,
+        loginUser,
+        currentUser,
+        logoutUser,
+        loadingUserInfo
+    }
 }
