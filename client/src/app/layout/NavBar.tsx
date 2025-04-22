88159f97cbfbfe00d9c2c2bccf8f1d34220a733e
allisoneur88 <adganshin@gmail.com>
2025-04-22 09:57:37 +0200

    16_01_Working on Photo Upload on client side

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -9,64 +9,64 @@ import UserMenu from "./UserMenu";
 
 export default function NavBar() {
 
-    const { uiStore } = useStore();
+   const { uiStore } = useStore();
 
-    const { currentUser } = useAccount();
+   const { currentUser } = useAccount();
 
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" sx={{
-                backgroundImage: "linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)",
-                position: "relative"
-            }}>
-                <Container maxWidth="xl">
-                    <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-                        <Box>
-                            <MenuItem component={NavLink} to="/" sx={{ display: "flex", gap: 2 }}>
-                                <Group fontSize="large" />
-                                <Typography variant="h4" fontWeight="bold">MyReactivities</Typography>
-                            </MenuItem>
-                        </Box>
-                        <Box sx={{ display: "flex" }}>
-                            <MenuItemLink to="/activities">
-                                Activities
-                            </MenuItemLink>
-                            <MenuItemLink to="/counter" >
-                                Counter
-                            </MenuItemLink>
-                            <MenuItemLink to="/errors" >
-                                Errors
-                            </MenuItemLink>
-                        </Box>
-                        <Box display="flex" alignItems="center">
-                            {currentUser ? (
-                                <UserMenu />
-                            ) : (
-                                <>
-                                    <MenuItemLink to="/login">Login</MenuItemLink>
-                                    <MenuItemLink to="/register">Register</MenuItemLink>
-                                </>
-                            )}
-                        </Box>
-                    </Toolbar>
-                </Container>
+   return (
+      <Box sx={{ flexGrow: 1 }}>
+         <AppBar position="static" sx={{
+            backgroundImage: "linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)",
+            position: "relative"
+         }}>
+            <Container maxWidth="xl">
+               <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+                  <Box>
+                     <MenuItem component={NavLink} to="/" sx={{ display: "flex", gap: 2 }}>
+                        <Group fontSize="large" />
+                        <Typography variant="h4" fontWeight="bold">MyReactivities</Typography>
+                     </MenuItem>
+                  </Box>
+                  <Box sx={{ display: "flex" }}>
+                     <MenuItemLink to="/activities">
+                        Activities
+                     </MenuItemLink>
+                     <MenuItemLink to="/counter" >
+                        Counter
+                     </MenuItemLink>
+                     <MenuItemLink to="/errors" >
+                        Errors
+                     </MenuItemLink>
+                  </Box>
+                  <Box display="flex" alignItems="center">
+                     {currentUser ? (
+                        <UserMenu />
+                     ) : (
+                        <>
+                           <MenuItemLink to="/login">Login</MenuItemLink>
+                           <MenuItemLink to="/register">Register</MenuItemLink>
+                        </>
+                     )}
+                  </Box>
+               </Toolbar>
+            </Container>
 
-                <Observer>
-                    {() => uiStore.isLoading ? (
-                        <LinearProgress
-                            color="secondary"
-                            sx={{
-                                position: "absolute",
-                                bottom: 0,
-                                left: 0,
-                                right: 0,
-                                height: 4
-                            }}
-                        />
-                    ) : null}
-                </Observer>
+            <Observer>
+               {() => uiStore.isLoading ? (
+                  <LinearProgress
+                     color="secondary"
+                     sx={{
+                        position: "absolute",
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        height: 4
+                     }}
+                  />
+               ) : null}
+            </Observer>
 
-            </AppBar>
-        </Box >
-    )
+         </AppBar>
+      </Box >
+   )
 }
--- client/src/app/layout/UserMenu.tsx
+++ client/src/app/layout/UserMenu.tsx
@@ -10,67 +10,71 @@ import { Add, Logout, Person } from '@mui/icons-material';
 
 export default function BasicMenu() {
 
-    const { currentUser, logoutUser } = useAccount();
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+   const { currentUser, logoutUser } = useAccount();
+   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+   const open = Boolean(anchorEl);
+   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+   };
+   const handleClose = () => {
+      setAnchorEl(null);
+   };
 
-    return (
-        <>
-            <Button
-                onClick={handleClick}
-                color="inherit"
-                size='large'
-                sx={{ fontSize: "1.1rem" }}
-            >
-                <Box display="flex" alignItems="center" gap={2}>
-                    <Avatar />
-                    {currentUser?.displayName}
-                </Box>
-            </Button>
-            <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                    'aria-labelledby': 'basic-button',
-                }}
-            >
-                <MenuItem component={Link} to="/createActivity" onClick={handleClose}>
-                    <ListItemIcon>
-                        <Add />
-                    </ListItemIcon>
-                    <ListItemText>
-                        Create Activity
-                    </ListItemText>
-                </MenuItem>
-                <MenuItem component={Link} to="/profile" onClick={handleClose}>
-                    <ListItemIcon>
-                        <Person />
-                    </ListItemIcon>
-                    <ListItemText>
-                        My Profile
-                    </ListItemText>
-                </MenuItem>
-                <Divider />
-                <MenuItem onClick={() => {
-                    logoutUser.mutate();
-                    handleClose();
-                }}>
-                    <ListItemIcon>
-                        <Logout />
-                    </ListItemIcon>
-                    <ListItemText>
-                        Logout
-                    </ListItemText>
-                </MenuItem>
-            </Menu>
-        </>
-    );
+   return (
+      <>
+         <Button
+            onClick={handleClick}
+            color="inherit"
+            size='large'
+            sx={{ fontSize: "1.1rem" }}
+         >
+            <Box display="flex" alignItems="center" gap={2}>
+               <Avatar
+                  src={currentUser?.imageUrl}
+                  alt='current user image'
+
+               />
+               {currentUser?.displayName}
+            </Box>
+         </Button>
+         <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+               'aria-labelledby': 'basic-button',
+            }}
+         >
+            <MenuItem component={Link} to="/createActivity" onClick={handleClose}>
+               <ListItemIcon>
+                  <Add />
+               </ListItemIcon>
+               <ListItemText>
+                  Create Activity
+               </ListItemText>
+            </MenuItem>
+            <MenuItem component={Link} to={`/profiles/${currentUser?.id}`} onClick={handleClose}>
+               <ListItemIcon>
+                  <Person />
+               </ListItemIcon>
+               <ListItemText>
+                  My Profile
+               </ListItemText>
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={() => {
+               logoutUser.mutate();
+               handleClose();
+            }}>
+               <ListItemIcon>
+                  <Logout />
+               </ListItemIcon>
+               <ListItemText>
+                  Logout
+               </ListItemText>
+            </MenuItem>
+         </Menu>
+      </>
+   );
 }
