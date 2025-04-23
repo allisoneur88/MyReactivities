701f5f6530639b0d420a91bc35c1f6044c2e425c
allisoneur88 <adganshin@gmail.com>
2025-04-23 22:25:24 +0200

    18_01_Working on adding SignalR for comments. Created Domain entity, configured DbContext, added handlers AddComment and GetComments

---
+++ Application/Activities/Queries/GetComments.cs
@@ -0,0 +1,34 @@
+using System;
+using Application.Activities.DTO;
+using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities.Queries;
+
+public class GetComments
+{
+   public class Querie : IRequest<Result<List<CommentDto>>>
+   {
+      public required string ActivityId { get; set; }
+   }
+
+   public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Querie, Result<List<CommentDto>>>
+   {
+      public async Task<Result<List<CommentDto>>> Handle(Querie request, CancellationToken cancellationToken)
+      {
+         var comments = await context.Comments
+            .Where(x => x.ActivityId == request.ActivityId)
+            .OrderByDescending(x => x.CreatedAt)
+            .ProjectTo<CommentDto>(mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+
+         return Result<List<CommentDto>>.Success(comments);
+
+      }
+   }
+}
--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -10,6 +10,7 @@ public class MappingProfiles : Profile
 {
    public MappingProfiles()
    {
+      // s for source, o for options, d for destination
       CreateMap<Activity, Activity>();
       CreateMap<CreateActivityDto, Activity>();
       CreateMap<EditActivityDto, Activity>();
@@ -27,5 +28,9 @@ public class MappingProfiles : Profile
           .ForMember(d => d.Id, o => o.MapFrom(s => s.User.Id));
 
       CreateMap<User, UserProfile>();
+      CreateMap<Comment, CommentDto>()
+         .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.User.DisplayName))
+         .ForMember(d => d.UserId, o => o.MapFrom(s => s.User.Id))
+         .ForMember(d => d.ImageUrl, o => o.MapFrom(s => s.User.ImageUrl));
    }
 }
